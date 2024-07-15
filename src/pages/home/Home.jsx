383a9c3f707250fe92fd9dd9c67cf2821af680dd
Tomás Serra <tomi.serra@gmail.com>
2024-07-15 01:57:49 -0300

    Estadisticas del juego

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Home.scss'
 
 import Logo from '../../assets/imgs/general/logo.png'
 import Image from '../../assets/imgs/general/image.png'
+import StatsViewer from '../../components/StatsViewer/StatsViewer'
+import SecretButton from '../../components/SecretButton/SecretButton'
+
 
 function Home({goToNextPage}) {
+  const [showStats, setShowStats] = useState(false)
+
   return (
     <div className='home-page'>
+      <div className="secret-button-container">
+        <SecretButton whenClicked={() => setShowStats(true)} totalClicks={2}/>
+      </div>
+      {showStats && <StatsViewer whenClose={()=>{setShowStats(false)}} />}
       <div className="header">
         <img src={Logo} />
       </div>
